a59d574dbd3e0d2224dcc3aaf2989f623adbad22
Redninjale <redninjale@gmail.com>
2024-03-25 22:51:11 -0500

    Quick Example

--- project-3-frontend/src/pages/About.js
+++ project-3-frontend/src/pages/About.js
@@ -1,9 +1,18 @@
+const Function2 = (props) => {
+    return(
+        <div className={props.name ? props.name : ""}>
+            <h1 className="p-[100px] text-lime-200">Function2</h1>
+            <p>This is the Function2 page</p>
+        </div>
+    );
+}
 
-const About = () => {
+const About = (props) => {
     return(
         <div>
-            <h1>About</h1>
+            <h1>About {props.name}</h1>
             <p>This is the about page</p>
+            <Function2 />
         </div>
     );
 }
