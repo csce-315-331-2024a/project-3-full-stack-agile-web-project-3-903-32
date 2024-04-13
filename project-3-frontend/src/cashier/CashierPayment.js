e0467b72fcda02f52a77166e919c545180f3bec5
vincentamu <123021332+vincentamu@users.noreply.github.com>
2024-04-12 21:32:32 -0500

    Update CashierPayment.js
    Cleaned up the white space

--- project-3-frontend/src/cashier/CashierPayment.js
+++ project-3-frontend/src/cashier/CashierPayment.js
@@ -81,77 +81,56 @@ const CashierPayment = () => {
     // background
     return (
         <div className='h-screen overflow-hidden bg-customMaroon'> {/* This set Background Color */}
-            
-            
             <div className="absolute inset-0 flex flex-col items-center justify-center overflow-hidden">
-                
-
                 <div className='w-full ml-64'>
-
-                <div className=' p-4 text-white text-6xl font-bold'> {/*This determinds the location of Payment text*/}
-                    <h1>Payment</h1>
-                </div>
-
-
-                <div className="w-1/2 p-4 bg-white rounded-lg shadow-md h-96 overflow-auto mr-80"> {/*This determinds the location */}
-                    <div className="flex flex-col items-right">
-                        <h2 className='mt-1 mb-2 font-semibold text-4xl'>Order List</h2>
-                        <div className='border-b border-black border-2 mb-2'></div>
+                    <div className=' p-4 text-white text-6xl font-bold'> {/*This determinds the location of Payment text*/}
+                        <h1>Payment</h1>
+                    </div>
+                    <div className="w-1/2 p-4 bg-white rounded-lg shadow-md h-96 overflow-auto mr-80"> {/*This determinds the location */}
+                        <div className="flex flex-col items-right">
+                            <h2 className='mt-1 mb-2 font-semibold text-4xl'>Order List</h2>
+                            <div className='border-b border-black border-2 mb-2'></div>
                 
-                        <div className="overflow-auto">
-                            {order.length > 0 ? (
-                                <ul>
-                                    {order.map((item,index) => (
-                                        <li key={item.id} className={`text-xl font-semibold mt-2 mb-2' ${index % 2 === 0 ? 'bg-white' : 'bg-gray-300'}`}>
-                                            {item.itemName} - ${item.price.toFixed(2)} x {item.quantity}
-                                        </li>
-                                    ))}
-                                </ul>
-                            ) : (
-                                <p className='text-xl'>No items in order.</p>
-                            )}
+                            <div className="overflow-auto">
+                                {order.length > 0 ? (
+                                    <ul>
+                                        {order.map((item,index) => (
+                                            <li key={item.id} className={`text-xl font-semibold mt-2 mb-2' ${index % 2 === 0 ? 'bg-white' : 'bg-gray-300'}`}>
+                                                {item.itemName} - ${item.price.toFixed(2)} x {item.quantity}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                ) : (
+                                    <p className='text-xl'>No items in order.</p>
+                                )}
+                            </div>
                         </div>
                     </div>
-                </div>
-                
             </div>
 
             <div className='absolute overflow-hidden bottom-22 right-10 mb-4 mr-8'>
                 <div className='flex flex-col items-center w-full'>
-                <div className='p-4 text-white text-3xl font-semibold'>
-                    <h3>Total: ${typeof total === 'number' ? total.toFixed(2) : '0.00'}</h3>
-                </div>
-
-                <div>
-                    <input 
-                        className="w-full mr-4 mb-4 overflow-y-auto py-2 px-8 bg-gray-50 rounded text-2xl"
-                        type="text" 
-                        placeholder="Enter customer's name" 
-                        value={name}
-                        onChange={changeName}
-                    />
-                </div>
-
+                    <div className='p-4 text-white text-3xl font-semibold'>
+                        <h3>Total: ${typeof total === 'number' ? total.toFixed(2) : '0.00'}</h3>
+                    </div>
+                    <div>
+                        <input 
+                            className="w-full mr-4 mb-4 overflow-y-auto py-2 px-8 bg-gray-50 rounded text-2xl"
+                            type="text" 
+                            placeholder="Enter customer's name" 
+                            value={name}
+                            onChange={changeName}
+                        />
+                    </div>
             </div>
                 {showEmptyMessage && <div>Order is empty</div>}
             <div className='flex flex-col'>
-
-                
                     <button onClick={toCashierSubmit} disabled={paymentSumbitted}className='w-full mr-4 overflow-y-auto py-2 px-8 bg-gray-50 rounded text-2xl'>Submit Payment</button>
-                
-
                     <button onClick={toCashierBack} className='w-full mt-4 mr-4 overflow-y-auto py-2 px-8 bg-gray-50 rounded text-2xl'>Back</button>
-
-            <button onClick={toCashierCancel} className='w-full mt-4 mr-4 overflow-y-auto py-2 px-8 bg-gray-50 rounded text-2xl'>Cancel</button>                
+                    <button onClick={toCashierCancel} className='w-full mt-4 mr-4 overflow-y-auto py-2 px-8 bg-gray-50 rounded text-2xl'>Cancel</button>                
             </div>
-            
             </div>
-
             </div>
-
-            
-    
-
     </div>
     );
 };
