320074b95b55b732060f859498a360f36d3c27a9
Jordan Daryanani <91863738+Jorsk@users.noreply.github.com>
2024-04-15 11:49:57 -0500

    test

--- project-3-frontend/src/App.js
+++ project-3-frontend/src/App.js
@@ -72,7 +72,7 @@ function App() {
   }
 
   const loginCustomer = () => {
-    navigate('/customer/StaticMenu');
+    navigate('/customer');
   }
 
 
@@ -97,7 +97,7 @@ function App() {
         />
       </div>
       <button 
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
         onClick={handleLogin}
       >
         Login
