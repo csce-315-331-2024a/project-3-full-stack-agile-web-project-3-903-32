31acc4468a6a0c1ba4c7870ff06e3b48fe7c5318
Jordan Daryanani <91863738+Jorsk@users.noreply.github.com>
2024-04-15 11:20:44 -0500

    fixed loginMenu()

--- project-3-backend/app.py
+++ project-3-backend/app.py
@@ -11,7 +11,7 @@ import deepl
 from collections import defaultdict
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": os.getenv('FRONTEND_URL')}})
+CORS(app)
 load_dotenv()
 
 # Database configuration
