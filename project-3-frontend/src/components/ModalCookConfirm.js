efbf059a97fad4ef0bdea2b445cf0fdc4b3b6c4e
vincentamu <123021332+vincentamu@users.noreply.github.com>
2024-04-28 18:15:10 -0500

    Fixed spelling plus misc. things

--- project-3-frontend/src/components/ModalCookConfirm.js
+++ project-3-frontend/src/components/ModalCookConfirm.js
@@ -5,11 +5,11 @@ const ModalCookConfirm = ({ isOpen, message, onConfirm, onCancel }) => {
         <>
             {isOpen &&
                 <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
-                    <div className="h-[200px] bg-white rounded-lg p-8">
-                        <p className="text-lg">{message}</p>
+                    <div className="h-[150px] bg-white rounded-lg p-8">
+                        <p className="text-lg font-semibold">{message}</p>
                         <div className="mt-8 flex justify-center">
-                            <button className="bg-blue-500 text-white px-4 py-2 rounded mr-2" onClick={onCancel}>Cancel</button>
-                            <button className="ml-16 bg-red-500 text-white px-4 py-2 rounded" onClick={onConfirm}>Confirm</button>
+                            <button className="font-semibold bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded mr-2" onClick={onCancel}>Cancel</button>
+                            <button className="font-semibold ml-16 bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded" onClick={onConfirm}>Confirm</button>
                         </div>
                     </div>
                 </div>
